b0453219233e256a3ad22a6caa407db52ee385b0
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-10-29 23:51:30 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
+require 'pry'
 class TweetsController < ApplicationController
+
+  get '/tweets' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      @tweets = Tweet.where(user_id: @user.id)
+      #binding.pry
+      erb :'/tweets/tweets'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      tweet = Tweet.new(content: params[:content])
+      #binding.pry
+      tweet.user = @user
+      tweet.save
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,49 @@
+require 'pry'
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    if !(params.has_value?(""))
+    @user = User.create(params)
+    session["user_id"] = @user.id
+    redirect to '/tweets'
+    else
+     redirect to '/signup' 
+    end
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    if !(params.has_value?(""))
+      @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+      session["user_id"] = @user.id
+      redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    if Helpers.is_logged_in?(session)
+      session.clear
+    else
+      redirect to '/'
+    end
+    redirect to '/login'
+  end
 
 end
